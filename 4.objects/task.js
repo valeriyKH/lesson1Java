fb2244eb15cc5a3b3331dbd6534f58740aa812b9
valera91k@yandex.ru <valera91k@yandex.ru>
2022-05-28 21:37:03 +0300

    Adde Object

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,44 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+const student1 = new Student("Ivan", "male", 30);
+const student2 = new Student("Petr", "male", 22);
+const student3 = new Student("Mari", "female", 20);
+const student4 = new Student("ann", "female", 34);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  this.mark = mark;
+}
+
+if(this.marks === undefined){ 
+    Student.marks += mark;// добавить первую оценку 
+    } else {
+     Student.marks += marks; // добавить вторую и последующие оценки в массив
+    }
+
+Student.prototype.addMarks = function (mark1, mark2, mark3) {
+  this.marks = (...marks)
+}
+
+Student.prototype.getAverage = function(){
+const average = marks.reduce((acc, item, idx, arr) => { if (idx === arr.length - 1) {
+return (acc + item) / arr.length; } else {
+    return acc + item;
+  }
+})
+}
+
+Student.prototype.exclude = function (reason) {
+  delete Student.subject;
+  delete Student.marks;
+  this.excluded = reason;
+}
+
+student1.setSubject("Algebra");
