81dac8b4bb34abe1494455b1ab0c1b879b8351fd
valera91k@yandex.ru <valera91k@yandex.ru>
2022-06-03 22:47:14 +0300

    Added task2, task3

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -28,38 +28,42 @@ console.log(getArrayParams([1, 2, 3, -100, 10]));
 
 
 // Задание 2
-function worker(arr) {
-  
-     for(let i= 0; i<arr.length; i++){
-       sum+=arr[i];
-     }
-     
-     return sum;
+function worker(arrOfArr){
+  let total = 0;
+  for(i= 0; i < arrOfArr.length; i++){
+    total += arrOfArr[i];
   }
-   
-function makeWork(arrOfArr,worker){
- let sums = arrOfArr[0];
- for(i = 0; i< arrOfArr.length; i++){
- 
- if(sums > arrOfArr[i]){
-   sums = worker(arrOfArr[i]);
-   }
- }
+  return total;
+}
+let arrOfArr = [[1,2,3,4], [10,20,-10,-20]];
+
+function makeWork(arrOfArr, worker){
+let sum = arrOfArr[0];
+for(let i=0; i< arrOfArr.length; i++){
+  sum = worker(arrOfArr[i])
+  }
+  if(sum < arrOfArr[i]){
+    let max = arrOfArr[i];
+  }
   return max;
 }
 
-// Задание 3
-function worker(arr){
-  sum = 0;
-   for(let i= 0; i<arr.length; i++){
-     sum+=arr[i];
-   }
-   
-   console.log(sum);
- }
- 
 
-function worker2(arr) {
-console.log(worker([4,5,6]));
+// Задание 3
+function worker2(arr){
+  this.arr = arr
+    min = arr[0];
+    max = arr[0];
+    for(let i = 0; i < arr.length; i++){
+      if(min > arr[i]){
+        min = arr[i];
+      } else if(max < arr[i]){
+        max = arr[i];
+      }
+      function difference (max, min){
+          console.log(Math.abs(max - min));   
+      }
+    }
 }
-worker2()
+
+console.log(worker2([1,2,3,4]))
