d25306db2b462fda47889b07036b6c93cb6b001a
Valeriy <valera91k@yandex.ru>
2022-06-08 15:40:56 +0300

    Object remote

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -11,31 +11,33 @@ const student4 = new Student("ann", "female", 34);
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
-}
+};
 
 Student.prototype.addMark = function (mark) {
-  if(this.marks == undefined) {
-    this.marks = [];
-  }
-  this.marks.push(mark);
-}
+  if(this.marks === undefined){ 
+    this.marks = []; 
+    } else {
+      this.marks.push(mark)
+    }
+};
 
-
-Student.prototype.addMarks = function (... addedMarks) {
-  if(this.marks == undefined) {
-    this.marks = [];
-  }
-  addedMarks.forEach(mark => this.marks.push(mark));
-} 
+Student.prototype.addMarks = function (...MARKS) {
+  if(this.marks === undefined){ 
+    this.marks = []; 
+    } else {
+      addedMarks.forEach(mark => this.marks.push(mark));
+    }
+};
 
 Student.prototype.getAvg  = function () {
   let sum = this.marks.reduce ((x, y) => x + y);
   let avg = sum / this.marks.length;
   return avg;
-}
+};
 
 Student.prototype.exclude = function (reason) {
   delete this.subject;
   delete this.marks;
   this.exclude = reason;
-}
+};
+
