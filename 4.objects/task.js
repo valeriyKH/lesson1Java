8baf084d441d18d76e8e3c7e542d63ff71d902d2
Valeriy <valera91k@yandex.ru>
2022-06-08 15:30:50 +0300

    Added Object

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,43 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+const student1 = new Student("Ivan", "male", 30);
+const student2 = new Student("Petr", "male", 22);
+const student3 = new Student("Mari", "female", 20);
+const student4 = new Student("ann", "female", 34);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = []; 
+    } else {
+      this.marks.push(mark)
+    }
+};
+
+Student.prototype.addMarks = function (...MARKS) {
+  if(this.marks === undefined){ 
+    this.marks = []; 
+    } else {
+      addedMarks.forEach(mark => this.marks.push(mark));
+    }
+};
+
+Student.prototype.getAvg  = function () {
+  let sum = this.marks.reduce ((x, y) => x + y);
+  let avg = sum / this.marks.length;
+  return avg;
+};
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.exclude = reason;
+};
+
