3f2ed308be5f830f39df24d30feee9f66fec5c80
valera91k@yandex.ru <valera91k@yandex.ru>
2022-06-12 22:48:55 +0300

    redackt task2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -29,41 +29,39 @@ console.log(getArrayParams([1, 2, 3, -100, 10]));
 
 // Задание 2
 function worker(arrOfArr){
-  let total = 0;
+  let total = null;
   for(i= 0; i < arrOfArr.length; i++){
     total += arrOfArr[i];
   }
   return total;
 }
+
 let arrOfArr = [[1,2,3,4], [10,20,-10,-20]];
 
-function makeWork(arrOfArr, worker){
-let sum = arrOfArr[0];
-for(let i=0; i< arrOfArr.length; i++){
-  sum = worker(arrOfArr[i])
-  }
-  if(sum < arrOfArr[i]){
-    let max = arrOfArr[i];
-  }
-  return max;
+function makeWork(arrOfArr, func) {
+  let max = -Infinity;
+    for(let i=0; i< arrOfArr.length; i++) {
+      let result = func(arrOfArr[i]);
+      if (max < result) {
+      max = result;
+      }
+        return max;
+      }
 }
 
-
 // Задание 3
-function worker2(arr){
-  this.arr = arr
-    min = arr[0];
-    max = arr[0];
-    for(let i = 0; i < arr.length; i++){
-      if(min > arr[i]){
-        min = arr[i];
-      } else if(max < arr[i]){
-        max = arr[i];
+function worker2(arrOfArr){
+  this.arr = arrOfArr
+    min = arrOfArr[0];
+    max = arrOfArr[0];
+    for(let i = 0; i < arrOfArr.length; i++){
+      if(min > arrOfArr[i]){
+        min = arrOfArr[i];
+      } else if(max < arrOfArr[i]){
+        max = arrOfArr[i];
       }
       function difference (max, min){
           console.log(Math.abs(max - min));   
       }
     }
 }
-
-console.log(worker2([1,2,3,4]))
